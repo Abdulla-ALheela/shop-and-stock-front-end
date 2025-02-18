d4162c7c8ed12af407d42ede1ff699fabe526b6c
Abdulla Alheela <Abdulla.alheela@gmail.com>
2025-02-18 19:50:54 +0300

    finish the purchase list

--- src/components/PurchaseLists/PurchaseLists.jsx
+++ src/components/PurchaseLists/PurchaseLists.jsx
@@ -1,7 +1,15 @@
 const PurchaseLists = (props) => {
 
     return (
+        <>
         <h1>purchase</h1>
+        {
+         props.lists.map((list) => (
+            list.listType === "Purchase list" ?
+            <p key={list._id}>{list.title}</p> : null
+          )) 
+        }
+          </>
     );
 };
 
